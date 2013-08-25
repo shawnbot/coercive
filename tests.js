dc4aae7d4a26ab85cfd1d23af59e1e06df6e52f1
Shawn Allen <shawn@stamen.com>
2013-08-25 14:51:29 -0400

    more tests

--- tests.js
+++ tests.js
@@ -1,10 +1,16 @@
 var vows = require("vows"),
     assert = require("assert"),
-    util = require("util"),
     coerce = require("./coercive");
 
 vows.describe("Coersions")
   .addBatch({
+
+    "numbers": {
+      topic: coerce.number()(5),
+      "should be numbers": function(n) {
+        assert(!isNaN(n));
+      }
+    },
 
     "numeric strings": {
       topic: function() {
@@ -173,6 +179,53 @@ vows.describe("Coersions")
       }
     },
 
+    // TODO more tests here
+
+  })
+  .addBatch({
+
+    "truthy strings": {
+      topic: function() {
+        var bool = coerce.boolean();
+        return [
+          "True",
+          "true",
+          "Yes",
+          "yes",
+          "T",
+          "t",
+          "1",
+          1
+        ].map(bool);
+      },
+      "should produce true": function(bools) {
+        bools.forEach(function(b) {
+          assert.strictEqual(b, true);
+        });
+      }
+    },
+
+    "falsy strings": {
+      topic: function() {
+        var bool = coerce.boolean();
+        return [
+          "False",
+          "false",
+          "No",
+          "no",
+          "F",
+          "f",
+          "0",
+          0
+        ].map(bool);
+      },
+      "should produce false": function(bools) {
+        bools.forEach(function(b) {
+          assert.strictEqual(b, false);
+        });
+      }
+    },
+
   })
   .addBatch({
 
@@ -193,6 +246,57 @@ vows.describe("Coersions")
     },
 
     // TODO more tests here
+
+  })
+  .addBatch({
+
+    "JSON object coersion": {
+      topic: function() {
+        var json = coerce.json();
+        return [
+          '{"foo":12}',
+          '{"baz":{"bar":1098234}}',
+        ].map(json);
+      },
+      "should produce objects": function(objs) {
+        objs.forEach(function(o) {
+          assert.equal(typeof o, "object");
+        });
+      }
+    },
+
+    "invalid JSON object coersion": {
+      topic: function() {
+        var json = coerce.json(null);
+        return [
+          '{foo:12}',
+          '{42}',
+        ].map(json);
+      },
+      "should produce null": function(objs) {
+        objs.forEach(function(o) {
+          assert.strictEqual(o, null);
+        });
+      }
+    },
+
+    "JSON numeric coersion": {
+      topic: function() {
+        var json = coerce.json();
+        return [
+          "14",
+          "135.86134",
+          "85298e-14",
+          "-28975",
+          "0.30823028"
+        ].map(json);
+      },
+      "should produce numbers": function(ns) {
+        ns.forEach(function(n) {
+          assert.equal(typeof n, "number");
+        });
+      }
+    },
 
   })
   .run();
